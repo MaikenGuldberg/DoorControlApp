247656a4b8a04bafddd4cd4ed53911a3a283a675
Maiken Guldberg <maiken.g@fibermail.dk>
2018-02-27 08:36:40 +0100

    Add project files.

---
+++ DoorControlApp.Test.Unit/DoorControlUnitTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace DoorControlApp.Test.Unit
+{
+    [TestFixture]
+    public class DoorControlUnitTests
+    {
+        private DoorControl _uut;
+
+        [SetUp]
+
+        public void SetUp()
+        {
+            _uut = new DoorControl();
+        }
+    }
+}
